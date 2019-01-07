dda47cf1d7edae1c52d866ff6f7a82e9697e7d04
x49QK2S25Jv <46139035+x49QK2S25Jv@users.noreply.github.com>
2019-01-07 21:02:10 +1300

    Create DNS-ResolutitionCheck.ps1

---
+++ DNS-ResolutitionCheck.ps1
@@ -0,0 +1,16 @@
+﻿$Website = Read-Host -Prompt "Please enter DNS Name"
+$PingWebsite = Resolve-DnsName pure-ip.com | Select -ExpandProperty IPAddress
+ipconfig /flushdns
+Clear
+
+while ($true) {
+$Date = Get-Date
+Write-Host "Current date is $Date" -ForegroundColor Gray
+Write-Host "Website is $Website" -ForegroundColor Green
+Write-Host "Website IP address is" -NoNewline
+Write-Host " $PingWebsite" -ForegroundColor Red
+Write-Host "Flushing DNS" -ForegroundColor Gray
+Ipconfig /flushdns | Out-Null
+Write-Host "===================================================" -ForegroundColor Red
+Sleep 5
+}
