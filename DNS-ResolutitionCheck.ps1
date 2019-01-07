1667d219cf8f9fd3f40815429ce898dbe16c4860
x49QK2S25Jv <46139035+x49QK2S25Jv@users.noreply.github.com>
2019-01-07 21:16:51 +1300

    Update DNS-ResolutitionCheck.ps1

--- DNS-ResolutitionCheck.ps1
+++ DNS-ResolutitionCheck.ps1
@@ -1,10 +1,10 @@
 ﻿$Website = Read-Host -Prompt "Please enter DNS Name"
-$PingWebsite = Resolve-DnsName pure-ip.com | Select -ExpandProperty IPAddress
 ipconfig /flushdns
 Clear
 
 while ($true) {
 $Date = Get-Date
+$PingWebsite = Resolve-DnsName google.com -type A | Select -ExpandProperty IPAddress
 Write-Host "Current date is $Date" -ForegroundColor Gray
 Write-Host "Website is $Website" -ForegroundColor Green
 Write-Host "Website IP address is" -NoNewline
