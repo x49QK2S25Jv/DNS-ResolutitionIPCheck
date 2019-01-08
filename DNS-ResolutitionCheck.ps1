35ca0a86ae59ba438905ff635ea588cc03c51485
x49QK2S25Jv <46139035+x49QK2S25Jv@users.noreply.github.com>
2019-01-08 20:49:43 +1300

    Update DNS-ResolutitionCheck.ps1

--- DNS-ResolutitionCheck.ps1
+++ DNS-ResolutitionCheck.ps1
@@ -1,13 +1,14 @@
 ﻿$DNSRecord = Read-Host -Prompt "Please enter DNS Name"
 ipconfig /flushdns
 Clear
+sleep 1
 
 while ($true) {
 $Date = Get-Date -Format "dd-MM-yyyy HH:mm:ss"
 $PingWebsite = Resolve-DnsName google.com -type A | Select -ExpandProperty IPAddress
 Write-Host "Current date is $Date" -ForegroundColor Gray
-Write-Host "DNS entered is $DNSRecord" -ForegroundColor Green
-Write-Host "Returned IP/s is " -NoNewline
+Write-Host "DNS requested is $DNSRecord" -ForegroundColor Green
+Write-Host "IP returned is" -NoNewline
 Write-Host " $PingWebsite" -ForegroundColor Red
 Write-Host "Flushing DNS" -ForegroundColor Gray
 Ipconfig /flushdns | Out-Null
