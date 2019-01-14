13c033daedb194f60c003f699cf1d323cf537ec1
x49QK2S25Jv <46139035+x49QK2S25Jv@users.noreply.github.com>
2019-01-14 13:09:28 +1300

    Update DNS-ResolutitionCheck.ps1

--- DNS-ResolutitionCheck.ps1
+++ DNS-ResolutitionCheck.ps1
@@ -1,12 +1,15 @@
 ﻿$DNSRecord = Read-Host -Prompt "Please enter DNS Name"
 ipconfig /flushdns
+ipconfig /all
 Clear
 sleep 1
 
 while ($true) {
 $Date = Get-Date -Format "dd-MM-yyyy HH:mm:ss"
+$DNSServer = ipconfig /all | Select-String -List 'DNS Servers'
 $PingWebsite = Resolve-DnsName google.com -type A | Select -ExpandProperty IPAddress
 Write-Host "Current date is $Date" -ForegroundColor Gray
+Write-Host "Primary IPV4 DNS server is $DNSServer" -ForegroundColor Gray
 Write-Host "DNS requested is $DNSRecord" -ForegroundColor Green
 Write-Host "IP returned is" -NoNewline
 Write-Host " $PingWebsite" -ForegroundColor Red
@@ -15,3 +18,5 @@ Ipconfig /flushdns | Out-Null
 Write-Host "===================================================" -ForegroundColor Red
 Sleep 5
 }
+
+Get-NetAdapterAdvancedProperty
